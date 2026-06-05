61ed0a2d2c08a3c9037ed7c1cc4a1a0025df6276
Kamaljeet Singh <sunny1968@live.in>
2026-06-05 17:48:53 +0900

    done

---
+++ go/2_Constants/3_if_condition.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+
+func main(){
+
+	var messagelen int = 20
+
+	if messagelen < 3{
+		fmt.Println("kill yourself")
+	
+	}else if messagelen == 2 {
+		fmt.Println("Kill yourself")
+	}else {
+		fmt.Println("kill me")
+	}
+
+	
+
+}
